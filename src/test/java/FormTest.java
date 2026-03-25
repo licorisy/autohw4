6760816154e69815f6352653231334b3606d3918
licoris <klepehin.k.a@gmail.com>
2026-03-25 15:10:04 +0300

    initial commit

---
+++ src/test/java/FormTest.java
@@ -0,0 +1,44 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class FormTest {
+    @BeforeEach
+    void setUp(){
+        Configuration.timeout = 15000;
+        Selenide.open("http://localhost:9999");
+    }
+    private String generateDate(long addDays,String pattern){
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    void shouldRegisterByNumber(){
+
+        $("[data-test-id='city'] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE)
+                        .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов Николай");
+        $("[data-test-id='phone'] input").setValue("+79996660000");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+
+    }
+
+}
